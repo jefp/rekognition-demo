cbab5b877c3804b4d55ffd6a596848bdbdd7e5ef
Jesús Federico <jefp@amazon.com>
2018-08-20 23:14:58 -0300

    Fixed install.sh file

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB8
 source ~/.rvm/scripts/rvm
 
 sudo yum install -y nodejs npm --enablerepo=epel
+sudo yum install -y openssl nginx
 
 rvm install 2.4.1 
 rvm gemset create aws-demo
@@ -13,3 +14,10 @@ gem install bundler
 cd ..
 cd -
 bundle
+
+openssl req -subj '/CN=demo.com/O=Amazon Rekognition/C=CL' -new -newkey rsa:2048 -sha256 -days 365 -nodes -x509 -keyout server.key -out server.crt
+mkdir -p /etc/nginx/ssl/certs/
+mv server.key server.crt /etc/nginx/ssl/certs/
+
+mv ssl.conf /etc/nginx/conf.d
+service nginx restart
